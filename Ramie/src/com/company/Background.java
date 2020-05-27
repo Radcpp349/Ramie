e056d164a1e3f43c2c43a0b34bef66eb18773c5d
Radosław <radcpp349@gmail.com>
2020-05-27 19:37:08 +0200

    Mamy większe okienko

--- Ramie/src/com/company/Background.java
+++ Ramie/src/com/company/Background.java
@@ -2,19 +2,39 @@ package com.company;
 
 import com.sun.j3d.utils.geometry.ColorCube;
 import com.sun.j3d.utils.universe.*;
-import javax.swing.JPopupMenu;
-import javax.swing.ToolTipManager;
+
+import javax.media.j3d.Canvas3D;
+import javax.media.j3d.Transform3D;
+import javax.swing.*;
 import javax.media.j3d.BranchGroup;
+import javax.vecmath.Vector3f;
+import java.awt.*;
 
 
-public class Background {
+public class Background extends JFrame {
 
 
     public Background() {
-        SimpleUniverse universe = new SimpleUniverse();
+        GraphicsConfiguration config =
+                SimpleUniverse.getPreferredConfiguration();
+
+        Canvas3D canvas3D = new Canvas3D(config);
+        canvas3D.setPreferredSize(new Dimension(800,600));
+
+        add(canvas3D);
+        pack();
+        setVisible(true);
+        SimpleUniverse universe = new SimpleUniverse(canvas3D);
         BranchGroup group = new BranchGroup();
         group.addChild(new ColorCube(0.5f));
+        group.compile();
         universe.getViewingPlatform().setNominalViewingTransform();
+
+        Transform3D przesuniecie_obserwatora = new Transform3D();
+        przesuniecie_obserwatora.set(new Vector3f(0.0f,0.0f,3.0f));
+
+        universe.getViewingPlatform().getViewPlatformTransform().setTransform(przesuniecie_obserwatora);
+
         universe.addBranchGraph(group);
     }
 /*
