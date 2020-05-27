8a9bb6368630aece2ddcb9ff817525c07518c78f
Radosław <radcpp349@gmail.com>
2020-05-27 16:31:59 +0200

    1kl..

--- Ramie/src/com/company/Main.java
+++ Ramie/src/com/company/Main.java
@@ -9,20 +9,15 @@ import javax.swing.*;
 
 public class Main {
 
-    public Main(){
-        SimpleUniverse universe = new SimpleUniverse();
-        BranchGroup group = new BranchGroup();
-        group.addChild(new ColorCube(0.5f));
-        universe.getViewingPlatform().setNominalViewingTransform();
-        universe.addBranchGraph(group);
-    }
+
 
     public static void main(String[] args) {
 
         System.setProperty("sun.awt.noerasebackground","true");
-        JPopupMenu.setDefaultLightWeightPopupEnabled(false);
+       JPopupMenu.setDefaultLightWeightPopupEnabled(false);
         ToolTipManager ttm=ToolTipManager.sharedInstance();
-        ttm.setLightWeightPopupEnabled(false);
-        new Main();
+       ttm.setLightWeightPopupEnabled(false);
+        Background obiekt = new Background();
+
     }
 }
