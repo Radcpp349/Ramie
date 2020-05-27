b5c63d15fc55d66cb3accea7815ca1b968f4c3ea
Radosław <radcpp349@gmail.com>
2020-05-27 15:58:46 +0200

    1kl

--- Ramie/src/com/company/Main.java
+++ Ramie/src/com/company/Main.java
@@ -1,9 +1,28 @@
 package com.company;
 
+
+import com.sun.j3d.utils.geometry.ColorCube;
+import com.sun.j3d.utils.universe.SimpleUniverse;
+
+import javax.media.j3d.BranchGroup;
+import javax.swing.*;
+
 public class Main {
 
+    public Main(){
+        SimpleUniverse universe = new SimpleUniverse();
+        BranchGroup group = new BranchGroup();
+        group.addChild(new ColorCube(0.5f));
+        universe.getViewingPlatform().setNominalViewingTransform();
+        universe.addBranchGraph(group);
+    }
+
     public static void main(String[] args) {
-	// write your code here
-    	System.out.println("Hello World!");
+
+        System.setProperty("sun.awt.noerasebackground","true");
+        JPopupMenu.setDefaultLightWeightPopupEnabled(false);
+        ToolTipManager ttm=ToolTipManager.sharedInstance();
+        ttm.setLightWeightPopupEnabled(false);
+        new Main();
     }
 }
