c91f631cab80525943959b09561376bf7c355a34
Oskar Fąk <oskarito111@onet.pl>
2020-06-21 23:23:36 +0200

    Poprawione ramie, usuwanie zbędnych linijek i bla bla bla

--- Ramie/src/com/company/Ramie.java
+++ Ramie/src/com/company/Ramie.java
@@ -30,10 +30,7 @@ import com.sun.j3d.utils.image.TextureLoader;
 public class Ramie extends JFrame {
 
 	private boolean klawisze[];
-
-    private Timer zegar = new Timer();
-    //private ViewingPlatform vPlatform;
-    
+    private Timer zegar = new Timer(); 
     private boolean CzyNagrywa = false;
     private boolean przenoszenie = false;
     private boolean odtworz_ruch = false;
@@ -48,9 +45,6 @@ public class Ramie extends JFrame {
     private float α_podstawa = 0f; // kąt przesunięcia bazy robota
     private float α_przegub = 0f; // kat przesuniecia ramienia1
     private float α_przegub2 = 0f;
-	    
-    private Vector3f positionramie = new Vector3f();
-    private Vector3f position = new Vector3f();
     
     ArrayList <PozycjaRobota> nagranie = new ArrayList<PozycjaRobota>();
     int klatka = 0;
@@ -70,8 +64,6 @@ public class Ramie extends JFrame {
     private TransformGroup t_sphere2 = new TransformGroup(p_sphere2);
     private Transform3D  p_KuliDolnej   = new Transform3D();
     private TransformGroup transformacja_s2 = new TransformGroup(p_KuliDolnej);
-    //private TransformGroup transformacja_s2;
-    //private Transform3D  p_KuliDolnej;
     
 	public Ramie(){
 
@@ -100,7 +92,6 @@ public class Ramie extends JFrame {
 
 		BranchGroup wezel_scena = new BranchGroup();
 
-
 		/* ustawiamy materiały, póiej zmiana w tekstury--------------------------------------------*/
 		Material zielonkawy = new Material();
 		zielonkawy.setDiffuseColor(0.9f,0.3f,0.3f);
@@ -118,7 +109,6 @@ public class Ramie extends JFrame {
 		p_gen.setTranslation(new Vector3f(-4.0f, -4.0f, 0.0f));
 		TransformGroup t_gen = new TransformGroup (p_gen);
 		t_gen.setCapability(TransformGroup.ALLOW_TRANSFORM_WRITE);
-
 
 		///KOD TESTOWY OD KAMILA, TRZEBA POUSTAWIAĆ
 
@@ -139,8 +129,6 @@ public class Ramie extends JFrame {
 		t_podstawy.addChild(podstawa);
 		t_gen.addChild(t_podstawy);
 
-
-
 		//walec ruchomy-----------------------------------------------------------//
 		Appearance  wygladCylindra = new Appearance();
 
@@ -152,12 +140,9 @@ public class Ramie extends JFrame {
 		p_walca.setTranslation(new Vector3f(0.0f, 3.5f, 0.0f));
 		TransformGroup t_walca = new TransformGroup (p_walca);
 		Cylinder walec = new Cylinder(0.7f, 6.4f,Primitive.GENERATE_TEXTURE_COORDS | Primitive.GENERATE_NORMALS | Primitive.GENERATE_NORMALS_INWARD, wygladCylindra);
-
 		
 		p_walca_2.setTranslation(new Vector3f(0.0f, 0.0f, 0.0f));
-		//TransformGroup t_walca_2 = new TransformGroup (p_walca_2);
 		t_walca_2.setCapability(TransformGroup.ALLOW_TRANSFORM_WRITE);
-
 
 		t_walca_2.addChild(t_walca);
 		t_podstawy.addChild(t_walca_2);
@@ -173,32 +158,23 @@ public class Ramie extends JFrame {
 
 		//ramię----------------------------------------------------------------------//
 
-
-
 		Appearance  wygladRamienia1 = new Appearance();
 		Texture texRamie1 = new TextureLoader( "blacha.jpeg", this).getTexture();
 		if (texRamie1 != null)
 		{wygladRamienia1.setTexture(texRamie1); }
-
 
 		Cylinder zaok1_ram = new Cylinder(0.6f, 1.36f, Primitive.GENERATE_TEXTURE_COORDS | Primitive.GENERATE_NORMALS | Primitive.GENERATE_NORMALS_INWARD,wygladRamienia1);
 
 		Transform3D p_zaok1_ram = new Transform3D();
 		p_zaok1_ram.setTranslation(new Vector3f(0.0f, 3.2f, 1.3f));
 		TransformGroup t_zaok1_ram = new TransformGroup (p_zaok1_ram);
-
-
-
 		
 		p_zaok1_ram_2.setTranslation(new Vector3f(0.0f, 0.f, 0.0f));
-		//TransformGroup t_zaok1_ram_2 = new TransformGroup (p_zaok1_ram_2);
 		t_zaok1_ram_2.setCapability(TransformGroup.ALLOW_TRANSFORM_WRITE);
 
 		Transform3D p_zaok1_ram_3 = new Transform3D();
 		p_zaok1_ram_3.setTranslation(new Vector3f(0.0f, 0.0f, 0.0f));
 		TransformGroup t_zaok1_ram_3 = new TransformGroup (p_zaok1_ram_3);
-
-
 
 		t_walca.addChild(t_zaok1_ram);
 		t_zaok1_ram.addChild(t_zaok1_ram_2);
@@ -206,8 +182,7 @@ public class Ramie extends JFrame {
 
 		t_zaok1_ram_3.addChild(zaok1_ram);
 
-		//t_przesunieta2_ram.addChild(t_zaok1_ram);
-		com.sun.j3d.utils.geometry.Box ram = new com.sun.j3d.utils.geometry.Box(2.8f, 0.6f, 0.18f,Primitive.GENERATE_TEXTURE_COORDS | Primitive.GENERATE_NORMALS | Primitive.GENERATE_NORMALS_INWARD, wygladRamienia1);
+		Box ram = new com.sun.j3d.utils.geometry.Box(2.8f, 0.6f, 0.18f,Primitive.GENERATE_TEXTURE_COORDS | Primitive.GENERATE_NORMALS | Primitive.GENERATE_NORMALS_INWARD, wygladRamienia1);
 
 		Transform3D p_przesuniety1_ram = new Transform3D();
 		p_przesuniety1_ram.setTranslation(new Vector3f(2.0f, 0.0f, 0.5f));
@@ -218,37 +193,17 @@ public class Ramie extends JFrame {
 		p_przesuniety0_ram.setTranslation(new Vector3f(0.0f, 0.0f, 0.0f));
 
 		TransformGroup t_przesunieta0_ram = new TransformGroup (p_przesuniety0_ram);
-		//t_przesunieta0_ram.setCapability(TransformGroup.ALLOW_TRANSFORM_WRITE);
 
 		Transform3D p_przesuniety2_ram = new Transform3D();
 		p_przesuniety2_ram.setTranslation(new Vector3f(0.8f, 0.f, 0.0f));
 		TransformGroup t_przesunieta2_ram = new TransformGroup (p_przesuniety2_ram);
 
-
-		//t_walca.addChild(t_przesunieta1_ram);
 		t_zaok1_ram_3.addChild(t_przesunieta1_ram);
 		t_przesunieta1_ram.addChild(t_przesunieta0_ram);
 		t_przesunieta0_ram.addChild(t_przesunieta2_ram);
 
 		t_przesunieta2_ram.addChild(ram);
-
-
-		//zaokrąglenie ramienia------------------------------------------------------------//
-		//Zmieniłem cylindry z 0.36f do 1.36f, obrotu e sie na siebie nakłądają niestety :(------------//
-	      /*Cylinder zaok1_ram = new Cylinder(0.6f, 1.36f, wygladRamienia1);
-	      Transform3D p_zaok1_ram = new Transform3D();
-	      p_zaok1_ram.setTranslation(new Vector3f(-2.8f, 0.f, 0.0f));
-	      TransformGroup t_zaok1_ram = new TransformGroup (p_zaok1_ram);
-	      t_zaok1_ram.addChild(zaok1_ram);
-	      t_przesunieta2_ram.addChild(t_zaok1_ram);
-	      */
-	      /*Cylinder zaok2_ram = new Cylinder(0.6f, 1.36f, wygladRamienia1);
-	      Transform3D p_zaok2_ram = new Transform3D();
-	      p_zaok2_ram.setTranslation(new Vector3f(2.6f, 0.f, 0.0f));
-	      TransformGroup t_zaok2_ram = new TransformGroup (p_zaok2_ram);
-	      t_zaok2_ram.addChild(zaok2_ram);
-	      t_przesunieta2_ram.addChild(t_zaok2_ram);*/
-
+		
 		//przedramię----------------------------------------------------------------------//
 		Appearance  wygladRamienia2 = new Appearance();
 
@@ -260,10 +215,8 @@ public class Ramie extends JFrame {
 		Transform3D p_przesuniety1_ram2 = new Transform3D();
 		p_przesuniety1_ram2.setTranslation(new Vector3f(2.8f, 0.0f, 0.9f));
 		TransformGroup t_przesunieta1_ram2 = new TransformGroup (p_przesuniety1_ram2);
-
 		
 		p_przesuniety0_ram2.setTranslation(new Vector3f(0.0f, 0.0f, 0.0f));
-		//TransformGroup t_przesunieta0_ram2 = new TransformGroup (p_przesuniety0_ram2);
 		t_przesunieta0_ram2.setCapability(TransformGroup.ALLOW_TRANSFORM_WRITE);
 
 		Transform3D p_przesuniety2_ram2 = new Transform3D();
@@ -286,15 +239,6 @@ public class Ramie extends JFrame {
 		t_przesunieta2_ram2.addChild(t_zaok1_ram2);
 		
 		t_przesunieta2_ram2.setBounds(new BoundingBox(new Point3d(0.0d,3.2d, 0.0d), new Point3d(5.0d ,3.6d, 0.4d)));
-		
-		
-		/*Appearance  wygladChwytak = new Appearance();
-
-		Texture texChwytak= new TextureLoader( "blacha.jpeg", this).getTexture();
-		if (texChwytak != null)
-		{wygladChwytak.setTexture(texChwytak); }*/
-
-		//sphere2 = new Sphere(0.6f,Primitive.GENERATE_TEXTURE_COORDS | Primitive.GENERATE_NORMALS | Primitive.GENERATE_NORMALS_INWARD, wygladChwytak);
 		
 		p_sphere2 = new Transform3D();
 		p_sphere2.setTranslation(new Vector3f(3.0f, 0.f, 0.0f));
@@ -327,39 +271,11 @@ public class Ramie extends JFrame {
 
 		///KONIEC KODU
 
-		/// Interakcja z robotem, obroty, nie działą obrót 1 ramienia tak jak powinien-----update 19.06, już działa---------------------//
-
-		/*Obrot_podstawy_robota obrot_podstawy = new Obrot_podstawy_robota(t_walca_2);
-		obrot_podstawy.setSchedulingBounds(new BoundingSphere());
-		wezel_scena.addChild(obrot_podstawy);
-
-		Obrot_ramienia obrot_ramienia = new Obrot_ramienia(t_zaok1_ram_2);
-		obrot_ramienia.setSchedulingBounds(new BoundingSphere());
-		wezel_scena.addChild(obrot_ramienia);
-
-		Obrot_ramienia_2 obrot_ramienia_2 = new Obrot_ramienia_2(t_przesunieta0_ram2);
-		obrot_ramienia_2.setSchedulingBounds(new BoundingSphere());
-		wezel_scena.addChild(obrot_ramienia_2);*/
-		
-		/*Grabbed grabbed = new Grabbed(t_przesunieta2_ram2,transformacja_s2);
-		grabbed.setSchedulingBounds(new BoundingSphere());
-		wezel_scena.addChild(grabbed);*/
-
 		Point3d point = new Point3d(11.4d, 1.8d, 0.3d); ///Zmieniłem!!!
 
-		/*KulaDolna.setBounds(new BoundingSphere(point, 0.1d));
+		KulaDolna.setBounds(new BoundingSphere(point, 0.1d));
 		Coll coll = new Coll(KulaDolna, KulaDolna.getBounds());
-		wezel_scena.addChild(coll);*/
-
-		/* do nagrywania, nie działa bo to kurwa jebana
-		Matrix3f matrix = new Matrix3f();
-		Vector3f vect = new Vector3f();
-		if(recordingON && temp1) {
-			p_walca_2.get(matrix, vect);
-			
-			temp1 = false;
-		}
-		*/
+		wezel_scena.addChild(coll);
 		
 		return wezel_scena;
 		
@@ -474,105 +390,6 @@ public class Ramie extends JFrame {
     }
 	
 
-	//obrot podstawy robota prawo-lewo-------------------------------------------------------------------------------//
-	/*public class Obrot_podstawy_robota extends Behavior{
-		
-		
-		
-
-		Obrot_podstawy_robota(TransformGroup th){
-			
-		}
-		@Override
-		public void initialize() {
-			this.wakeupOn(new WakeupOnAWTEvent(KeyEvent.KEY_PRESSED));
-		}
-		@Override
-		public void processStimulus(Enumeration enmrtn) {
-			if(klawisze[0]&&(α_podstawa<3.14)) 
-				α_podstawa += 0.03f;
-				
-			if(klawisze[2]&&(α_podstawa>-3.14)) 
-				α_podstawa -=0.03f;
-				
-			
-			this.wakeupOn(new WakeupOnAWTEvent(KeyEvent.KEY_PRESSED));
-		}
-	}
-
-	//obrot ramienia 2 robota góra-dół--------------------------------------------------------------------------------//
-	public class Obrot_ramienia_2 extends Behavior{
-		
-
-		Obrot_ramienia_2 (TransformGroup th){
-			
-		}
-		@Override
-		public void initialize() {
-			this.wakeupOn(new WakeupOnAWTEvent(KeyEvent.KEY_PRESSED));
-		}
-		@Override
-		public void processStimulus(Enumeration enmrtn) {
-			if(klawisze[3]&&(α_przegub<2.85)) 
-				 α_przegub +=0.03f;
-				
-			if(klawisze[5]&&(α_przegub>-2.85)) 
-				α_przegub -=0.03f;
-				
-			this.wakeupOn(new WakeupOnAWTEvent(KeyEvent.KEY_PRESSED));
-		}
-
-
-
-	}
-
-	/// Obrót pierwszego ramienia robota góra-dół-----------------------------------///
-	public class Obrot_ramienia extends Behavior{
-		
-
-		Obrot_ramienia(TransformGroup th){
-
-
-		}
-		@Override
-		public void initialize() {
-			this.wakeupOn(new WakeupOnAWTEvent(KeyEvent.KEY_PRESSED));
-		}
-		@Override
-		public void processStimulus(Enumeration enmrtn) {
-			if(klawisze[1]&&(α_przegub2<3.14)) 
-				α_przegub2 += 0.03f;
-			
-			if(klawisze[4]&&(α_przegub2>-3.14)) 
-				α_przegub2 -= 0.03f;
-				
-			this.wakeupOn(new WakeupOnAWTEvent(KeyEvent.KEY_PRESSED));
-		}
-	}*/
-	
-	public class Grabbed extends Behavior{
-		
-		private TransformGroup tg1;
-		private TransformGroup tg2;
-		
-
-		Grabbed(TransformGroup th1,TransformGroup th2){
-			this.tg1 = th1;
-			this.tg2 = th2;
-
-		}
-		@Override
-		public void initialize() {
-			this.wakeupOn(new WakeupOnAWTEvent(KeyEvent.KEY_PRESSED));
-		}
-		@Override
-		public void processStimulus(Enumeration enmrtn) {
-			
-			
-			this.wakeupOn(new WakeupOnAWTEvent(KeyEvent.KEY_PRESSED));
-		}
-	}
-
 	
 	public class Coll extends Behavior {
 		/** The separate criteria used to wake up this beahvior. */
@@ -617,6 +434,7 @@ public class Ramie extends JFrame {
 		 * to the OR'ed criterion again.
 		 */
 		public void processStimulus(Enumeration criteria) {
+			if(criteria.hasMoreElements()){
 			WakeupCriterion theCriterion = (WakeupCriterion) criteria.nextElement();
 			if (theCriterion instanceof WakeupOnCollisionEntry) {
 				Node theLeaf = ((WakeupOnCollisionEntry) theCriterion)
@@ -635,6 +453,7 @@ public class Ramie extends JFrame {
 			}
 			wakeupOn(oredCriteria);
 		}
+			}
 	}
 	
 	
@@ -699,29 +518,37 @@ public class Ramie extends JFrame {
 		          t_zaok1_ram_2.setTransform(obrot3);
 		          t_przesunieta0_ram2.setTransform(obrot2);
 		          
+		         
 		          
-		          
-		          System.out.println("Poruszanie");
 		          transformacja_s2.setTransform(kula3D);
 		          sphere2.getLocalToVworld(p_sphere2);
 		          ram2.getLocalToVworld(p_przesuniety0_ram2);
-		          
+		          Vector3f position = new Vector3f();
 		          p_sphere2.get(position);
+		          Vector3f positionramie = new Vector3f();
 		          p_przesuniety0_ram2.get(positionramie);
 		           		         
 		          if (przenoszenie) {
-		                float f1 = 1.17f;
-		                float f2 = 0.17f;
-
+		                /*float f1 = 2.89f;
+		                float f2 = 1.04f;
+		                
+		               
+		                System.out.print(positionramie);
+		                
 		                pozycjaX = position.x * f1 - f2 * positionramie.x;
 		                pozycjaY = position.y * f1 - f2 * positionramie.y;
-		                pozycjaZ = position.z * f1 - f2 * positionramie.z;
-		                kula3D.set(new Vector3f(pozycjaX, pozycjaY, pozycjaZ));
+		                pozycjaZ = position.z * f1 - f2 * positionramie.z;*/
+		        	  System.out.print(position);
+		        	  System.out.println('\n');
+		        	    pozycjaX = position.x;
+		        	    pozycjaY = position.y;
+		        	    pozycjaZ = position.z;
+		                kula3D.setTranslation(new Vector3f(pozycjaX+0.8f, pozycjaY+0.8f, pozycjaZ));
 
 		            } else {
-		                if (pozycjaY > 0.3) {
-		                    pozycjaY -= 0.02f;
-		                    kula3D.set(new Vector3f(pozycjaX, pozycjaY, pozycjaZ));
+		                if (pozycjaY > 1.79f) {
+		                    pozycjaY -= 0.04f;
+		                    kula3D.setTranslation(new Vector3f(pozycjaX+0.8f, pozycjaY+0.8f, pozycjaZ));
 
 		                	}
 		              }
